05971966eb924a1f173885cb87101b10b34a1cc0
kangshy <kangshy@gmail.com>
2022-05-14 16:17:19 +0900

    use mixin

--- src/views/FirstView.vue
+++ src/views/FirstView.vue
@@ -7,28 +7,15 @@
 </template>
 
 <script>
-    // import mixins from '@/mixins'
+    import mixins from '@/mixins'
 
     export default {
 
-        // mixins : [mixins],
+        mixins : [mixins],
 
         data : () => ({
             yesterday : new Date(new Date().getTime() - 1000 * 60 * 60 * 24)
         }),
-
-        filters : {
-            t2f(dateObj){
-                const year = String(dateObj.getFullYear());
-                const month = String(dateObj.getMonth() + 1).padStart(2,0);
-                const date = String(dateObj.getDate()).padStart(2,0);
-                const hour = String(dateObj.getHours()).padStart(2,0);
-                const minute = String(dateObj.getMinutes()).padStart(2,0);
-                const second = String(dateObj.getSeconds()).padStart(2,0);
-                return `${year}-${month}-${date} ${hour}:${minute}:${second}`;
-            }
-        }
-
     }
 </script>
 
